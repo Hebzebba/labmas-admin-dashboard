4db6da86473220c6a2e435fd5cc6652eea3e5947
Hebzebba <kwashieseth34@gmail.com>
2022-06-09 11:18:07 +0000

    Modified admin login banner

--- src/views/adminform/Login.js
+++ src/views/adminform/Login.js
@@ -52,7 +52,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <div className="login-profile-img">Admin Login</div>
+        <h3>Admin</h3>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Email</label>
